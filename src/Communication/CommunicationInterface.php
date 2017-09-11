60156d015ceec3e16b6a5ef08b13e93405d0d842
Krzysztof Wędrowicz <krzysztof.wedrowicz@espeo.eu>
2017-09-11 08:40:16 +0200

    CommunicationInterface typing

--- src/Communication/CommunicationInterface.php
+++ src/Communication/CommunicationInterface.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace KWedrowicz\JobCommunication\Communication;
 
+use KWedrowicz\JobCommunication\Params\ParamsInterface;
+
 interface CommunicationInterface
 {
-    public function send($message): void;
+    public function send(ParamsInterface $message): void;
 }
